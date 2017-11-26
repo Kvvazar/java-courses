d56fe0772390b93de1581fe06a75cb91458d7664
Kvvazar <a.olejnik1992@gmail.com>
2017-11-26 15:48:39 +0300

    Calculate create

--- calculate.java
+++ calculate.java
@@ -1,5 +1,11 @@
+import static java.lang.Math.*;
 public class calculate {
     public static void main (String []arg) {
 	System.out.println("Calculate...");
+	int x = 4;
+	int y = 4;
+	int z = 4;
+	double a = pow(x, 2) * pow(y, 2) * sqrt(z);
+	System.out.println("Answer " + a);
     }
 }
